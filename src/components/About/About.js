5f65ec35c6e5b68f7d212b433ccbfc5ca8bf215a
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-04-30 01:29:43 -0400

    Skillset

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
-import './About.scss'
+import "./About.scss";
+import CV from '../../assets/NipunaGamageCV.pdf'
 
 function About() {
   const pageVariants = {
@@ -29,7 +30,7 @@ function About() {
   };
   return (
     <motion.div
-    className="about"
+      className="about"
       initial="initial"
       animate="animate"
       exit="exit"
@@ -42,11 +43,8 @@ function About() {
         Personal <span1 className="about__pers">Info</span1>
       </h2>
       <div className="about__info">
-      
         <div className="about__details">
-        
           <div className="about__first">
-            
             <p className="about__desc">
               First Name:<span className="about__desc-info">Nipuna</span>
             </p>
@@ -57,49 +55,178 @@ function About() {
               DOB: <span className="about__desc-info">1998/10/24</span>
             </p>
             <p className="about__desc">
-              Address:<span className="about__desc-info">725 greenoch Court Oshawa</span>
+              Address:
+              <span className="about__desc-info">
+                725 greenoch Court Oshawa
+              </span>
             </p>
             <p className="about__desc">
               Freelance: <span className="about__desc-info">Available</span>
             </p>
+            <a href={CV} download="Nipuna_Gamage_CV.pdf" class="about__download-btn">
+              <p className="about__download-name">Download CV</p>
+              
+              <span class="about__download-icon">&#x2B07;</span>
+            </a>
           </div>
           <div className="about_second">
             <p className="about__desc">
               Phone: <span className="about__desc-info">+16478945532</span>
             </p>
             <p className="about__desc">
-              Email: <span className="about__desc-info">nipunagamage888@gmail.com</span>
+              Email:
+              <span className="about__desc-info">
+                nipunagamage888@gmail.com
+              </span>
             </p>
             <p className="about__desc">
-              Address:<span className="about__desc-info">725 greenoch Court Oshawa</span>
+              Address:
+              <span className="about__desc-info">
+                725 greenoch Court Oshawa
+              </span>
             </p>
             <p className="about__desc">
-              Linkedin <span className="about__desc-info">dafasdadfadfdafadfdafadfda</span>
+              Linkedin{" "}
+              <span className="about__desc-info">
+                dafasdadfadfdafadfdafadfda
+              </span>
             </p>
             <p className="about__desc">
-              Address:<span className="about__desc-info">725 greenoch Court Oshawa</span>
+              Address:
+              <span className="about__desc-info">
+                725 greenoch Court Oshawa
+              </span>
             </p>
           </div>
         </div>
         <div className="about__years">
           <div className="about__exp">
-            <h1>12+</h1>
-            <p>____ years of Experience</p>
+            <h1 className="about__exp-heading">2+</h1>
+            <p className="about__exp-para"> years of Experience</p>
+            <p className="about__exp-para"> Front End Developer</p>
           </div>
           <div className="about__exp">
-            <h1>12+</h1>
-            <p>____ years of Experience</p>
+            <h1 className="about__exp-heading">1+</h1>
+            <p className="about__exp-para">years of Experience</p>
+            <p className="about__exp-para">Intern Full Stack developer</p>
           </div>
           <div className="about__exp">
-            <h1>12+</h1>
-            <p>____ years of Experience</p>
+            <h1 className="about__exp-heading">6+</h1>
+            <p className="about__exp-para">years of Experience</p>
+            <p className="about__exp-para">Hackathon Participations</p>
           </div>
           <div className="about__exp">
-            <h1>12+</h1>
-            <p>____ years of Experience</p>
+            <h1 className="about__exp-heading">4+</h1>
+            <p className="about__exp-para">years of Experience</p>
+            <p className="about__exp-para">Personal Projects</p>
           </div>
+        </div>
+      </div>
+      <div className="about__skills">
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+          <div className="about__chart">
+            <div className="about__progress">
+                <p className="about__percentage">
+                  20%
+                </p>
+                
+            </div>  
+            <p className="about__language">Html</p>
+          </div>
+         
+      </div>
+      <div className="about__final">
+        <div>
+        <h2>Education</h2>
+        <div className="about__first-ed">
+          <h2>Branstation, Toronto Ontario</h2>
+        </div>
+        </div>
+        <div>
+          <h2>Experience</h2>
           
         </div>
+        
       </div>
     </motion.div>
   );
