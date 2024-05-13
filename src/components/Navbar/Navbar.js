94fb21f2cd672f2caa90b99d37f775e8d674362e
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-05-13 19:53:54 -0400

    Creating mobile styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./Navbar.scss";
 import { useNavigate } from "react-router-dom/dist";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
   const navigate=useNavigate()
   return (
     <nav className="nav">
+      <div className="nav__mobile">
+        <FontAwesomeIcon
+          className="nav__icon"
+          icon={faBars}
+          style={{ color: "#ffb400" }}
+        />
+        <ul className="nav__list-mobile">
+        <li onClick={()=>navigate('/')} className="nav__li-mobile">
+            Home 
+        </li>
+        <li onClick={()=>navigate('/about')} className="nav__li-mobile">
+            About Me
+        </li>
+        <li onClick={()=>navigate('/Contact')} className="nav__li-mobile">
+            Contact
+        </li>
+        <li  onClick={()=>navigate('/projects')} className="nav__li-mobile"> 
+          Projects
+        </li>
+
+     
+      </ul>
+      </div>
       <ul className="nav__list">
         <li onClick={()=>navigate('/')} className="nav__li">
             Home 
