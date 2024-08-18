9c4647625de4e8c73a462033815b4e8cd99cf306
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-08-18 01:03:55 -0400

    project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Projects.scss'
+import parking from '../../assets/parking.jpeg'
 
 export default function Projects() {
   return (
     <div className='project'>
-        <p className='project__melwa'>Melwa</p>
+        <p className='project__melwa'>Projects</p>
+        <div className='project__info'>
+          <div className='project__more'>
+          <img className='project__image' alt='Image of a paring' src={parking}/>
+          </div>
+          <div className='project__more'>
+          <img className='project__image' alt='Image of a paring' src={parking}/>
+          </div>
+          <div className='project__more'>
+          <img className='project__image' alt='Image of a paring' src={parking}/>
+          </div>
+         
+        </div>
     </div>
   )
 }
