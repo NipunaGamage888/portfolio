26c24726cf95238e86f264a1f7dffe91db5ca60c
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-08-19 11:34:31 -0400

    project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,16 +13,16 @@ export default function Projects() {
             alt="Image of a paring"
             src={parking}
           />
-          <h2 className="project__name">Parking Genie</h2>
+          <h2 className="project__name">Attendee</h2>
           <p className="project__details">
-            An single website that helps people to search and book parking
-            spaces
+            An single website that helps the parking lot owners to manage all the
+            parkings available in their lot.
           </p>
           <button className="project__button">
             <a
               className="project__link"
               target="_blank"
-              href="https://github.com/NipunaGamage888/nipuna-gamage-capstone"
+              href="NipunaGamage888/nipuna-gamage-capstone-attendee"
             >
               View in GitHub
             </a>
@@ -34,16 +34,16 @@ export default function Projects() {
             alt="Image of a paring"
             src={parking}
           />
-          <h2 className="project__name">Parking Genie</h2>
+          <h2 className="project__name">BrainFlix</h2>
           <p className="project__details">
-            An single website that helps people to search and book parking
-            spaces
+            A Demo Website simillar to Youtube which allows the users to search
+            and watch movies in the site.
           </p>
           <button className="project__button">
             <a
               className="project__link"
               target="_blank"
-              href="https://github.com/NipunaGamage888/nipuna-gamage-capstone"
+              href="https://github.com/NipunaGamage888/nipuna-gamage-brainflix"
             >
               View in GitHub
             </a>
