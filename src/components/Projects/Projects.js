41b48704d00162e28ac63b62b35b1d2307601c55
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-08-19 11:05:54 -0400

    adding the info for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,76 @@
-import React from 'react'
-import './Projects.scss'
-import parking from '../../assets/parking.jpeg'
+import React from "react";
+import "./Projects.scss";
+import parking from "../../assets/parking.jpeg";
 
 export default function Projects() {
   return (
-    <div className='project'>
-        <p className='project__melwa'>Projects</p>
-        <div className='project__info'>
-          <div className='project__more'>
-          <img className='project__image' alt='Image of a paring' src={parking}/>
-          </div>
-          <div className='project__more'>
-          <img className='project__image' alt='Image of a paring' src={parking}/>
-          </div>
-          <div className='project__more'>
-          <img className='project__image' alt='Image of a paring' src={parking}/>
-          </div>
-         
+    <div className="project">
+      <p className="project__melwa">Projects</p>
+      <div className="project__info">
+        <div className="project__more">
+          <img
+            className="project__image"
+            alt="Image of a paring"
+            src={parking}
+          />
+          <h2 className="project__name">Parking Genie</h2>
+          <p className="project__details">
+            An single website that helps people to search and book parking
+            spaces
+          </p>
+          <button className="project__button">
+            <a
+              className="project__link"
+              target="_blank"
+              href="https://github.com/NipunaGamage888/nipuna-gamage-capstone"
+            >
+              View in GitHub
+            </a>
+          </button>
         </div>
+        <div className="project__more">
+          <img
+            className="project__image"
+            alt="Image of a paring"
+            src={parking}
+          />
+          <h2 className="project__name">Parking Genie</h2>
+          <p className="project__details">
+            An single website that helps people to search and book parking
+            spaces
+          </p>
+          <button className="project__button">
+            <a
+              className="project__link"
+              target="_blank"
+              href="https://github.com/NipunaGamage888/nipuna-gamage-capstone"
+            >
+              View in GitHub
+            </a>
+          </button>
+        </div>
+        <div className="project__more">
+          <img
+            className="project__image"
+            alt="Image of a paring"
+            src={parking}
+          />
+          <h2 className="project__name">Parking Genie</h2>
+          <p className="project__details">
+            An single website that helps people to search and book parking
+            spaces
+          </p>
+          <button className="project__button">
+            <a
+              className="project__link"
+              target="_blank"
+              href="https://github.com/NipunaGamage888/nipuna-gamage-capstone"
+            >
+              View in GitHub
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
